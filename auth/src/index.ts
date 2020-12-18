d5da89d3bc0708bd279b3cf267b5a9aa62cc4bf5
kirito-k <devavratk96@gmail.com>
2020-12-18 16:02:15 -0500

    Initial setup of Authentication service + K8s setup

---
+++ auth/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.json());
+
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Auth is listening on port ${port}!`)
+})
