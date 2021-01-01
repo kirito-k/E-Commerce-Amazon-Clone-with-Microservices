37f757ded3a6c0af23087a8f2dd7e21c78341c61
kirito-k <devavratk96@gmail.com>
2020-12-31 20:47:00 -0500

    Added Signout Logic by removing cookies

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,21 @@
+// Goal: This route allows React to determine whether a user is currently logged in our not. If the request cookie has a JWT token, the user is logged in other wise not.
+
+import jwt from "jsonwebtoken";
 import express from "express";
 
 const router = express.Router();
 
 router.get("/api/users/currentuser", (req, res) => {
-  res.send("Hello from currentUser")
-})
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
 
-export { router as currentUserRouter }
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    return res.send({ currentUser: payload });
+  } catch (err) {
+    return res.send({ currentUser: null });
+  }
+});
+
+export { router as currentUserRouter };
